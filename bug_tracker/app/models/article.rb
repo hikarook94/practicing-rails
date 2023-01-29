93b35a2bc339488889b3d1803e219827c834cda8
hikarook94 <walrus4l145@icloud.com>
2023-01-29 23:39:13 +0900

    ~ Some tips for investigating ideas through the Rails console

---
+++ bug_tracker/app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+  # WIPで保存した時
+  validates :author_id, presence: true
+  # 公開した時
+  validates :body, presence: true, on: :publish
+end
--- bug_tracker/db/schema.rb
+++ bug_tracker/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_14_103952) do
+ActiveRecord::Schema.define(version: 2023_01_29_143040) do
+
+  create_table "articles", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_articles_on_author_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "bugs", force: :cascade do |t|
     t.string "title"
