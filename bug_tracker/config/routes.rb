89e0854f163c59afc88979573112aefc59f207af
hikarook94 <walrus4l145@icloud.com>
2023-01-29 23:22:05 +0900

    ~ Where does the code go?

--- bug_tracker/config/routes.rb
+++ bug_tracker/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'bugs#index'
+  root 'bugs#index'
   resources :bugs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
